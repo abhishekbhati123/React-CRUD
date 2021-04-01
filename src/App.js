6be74893c4fcc1d0c9272b1ceb43ab33c55a3df0
abhishek <abhishekbhati1993@gmail.com>
2021-04-01 13:29:01 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import Contacts from './components/pages/Contacts';
+import Navbar from './components/pages/Navbar';
+
+import {BrowserRouter as Router,Route,Switch}  from 'react-router-dom';
+import AddUser from './components/Users/AddUsers';
+import NotFound from './components/pages/NotFound';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Router>
+      <div className="App">
+      <Navbar/>
+      <Switch>
+      <Route exact path="/" component={Home}></Route>
+      <Route path="/About" component={About}></Route>
+      <Route path="/Contacts" component={Contacts}></Route>
+      <Route path="/users/add"  component={AddUser}></Route>
+      <Route component={NotFound}></Route>
+      </Switch>
+     
+      
+     
     </div>
+     </Router>
   );
 }
 
---
+++ src/components/pages/Home.js
@@ -0,0 +1,57 @@
+import React, {useState,useEffect} from "react";
+import axios from "axios";
+import {Link} from 'react-router-dom';
+const Home = () => {
+    const [users,setUser]=useState([]);
+
+    useEffect(()=>{
+        loadUsers();
+    },[]);
+    
+    const loadUsers=async ()=>{
+        const result=await axios.get("http://localhost:3003/users")
+        
+        
+        setUser(result.data);
+
+    }
+  return (
+    <div className="Container">
+      <div className="py-4">
+        <h1>Home</h1>
+        <table class="table border shadow">
+          <thead className="thead-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">User Name</th>
+              <th scope="col">Email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+                users.map((user,index)=>(
+                    <tr>
+                        <th scope="row">{index+1}</th>
+                        <td>{user.name}</td>
+                        <td>{user.username}</td>
+                        <td>{user.email}</td>
+                        <td>
+                            <Link className="btn btn-primary">View</Link>
+                            <Link className="btn btn-outline-primary">Edit</Link>
+                            <Link className="btn btn-danger">Delete</Link>
+                        </td>
+                    </tr>
+                )
+
+                )
+            }
+     
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+export default Home;
